2e90108ef223eac50a06b1af278dfe5f214c1d18
Stuart Dunning <stuart@bearclawsolutions.co.uk>
2017-09-08 14:13:42 +0100

    Update config required for application to run

--- Database/Config/config.sql
+++ Database/Config/config.sql
@@ -1,20 +1,34 @@
 
-update config set config_data = '{
+insert into config (app_id, config_id, config_data)
+values ('data-sharing-manager','application',
+      '{ "appUrl" : "http://localhost:8080" }'
+      );
+
+insert into config (app_id, config_id, config_data)
+values ('data-sharing-manager','database',
+      '{
+   "url" : "jdbc:mysql://localhost:3306/data_sharing_manager",
+   "username" : "YOURUSRNAME",
+   "password" : "YOURPASSWORD"
+}'
+);
+
+insert into config (app_id, config_id, config_data)
+values ('data-sharing-manager','GoogleMapsAPI',
+      '{
+   "url" : "https://maps.googleapis.com/maps/api/geocode/json?address=",
+   "apiKey" : "AIzaSyD0vq83Q9bjIQH25R64p5RuCquDo56gP0Y"
+}'
+      );
+
+insert into config (app_id, config_id, config_data)
+values ('data-sharing-manager','keycloak',
+      '{
   "realm": "endeavour",
   "realm-public-key": "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7GdjckqAZgjxp/o7717ei5RgkW3mtG3W+LfmlboBt20NQ/Jz6yb00Xoe9dBCLsqiiompePWuBNxGdwUNHzJcng7hpTvsi7Zp8PtTJDts/EinroKEv+Gac2VB1k8aLneDOtU6FYdi7uQ4vVU4xJ4D4s1ubG0VQXqUnSUvwwRN5UDdGYLrV2KueajgsNJ3mML4aJ2rLDyUF5uvKQV1UbZAwvCUo0tIeUYoN6PMkpaUrBagWeLhNhrNU9HsiDbMUjVttDRgMlgCtYvu4GapI+0cVecAUWfg0MdTCYuUJwUtTZoatf3d2bietsS+cYPFfs9eCIm1/7GLZWwv6qFDN1a4ewIDAQAB",
   "auth-server-url": "https://devauth.endeavourhealth.net/auth",
   "ssl-required": "external",
-  "resource": "eds-ui",
+  "resource": "eds-dsa-manager",
   "public-client": true
 }'
-where app_id = 'eds-patient-explorer'
-and config_id = 'keycloak'
-
-insert into config (app_id, config_id, config_data)
-values ('eds-information-modeller','informationmodelDB',
-      '{
-   "url" : "jdbc:mysql://localhost:3306/information_model",
-   "username" : "root",
-   "password" : "admin"
-}')  
-
+);
