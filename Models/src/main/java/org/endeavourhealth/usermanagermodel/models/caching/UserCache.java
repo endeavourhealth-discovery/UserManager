e2a5e54e6c77586fe740a58cab329621cc3e1a06
stuart <stuart@bearclawsolutions.co.uk>
2018-10-01 13:51:10 +0100

    Add a load all users options to the cache

--- Models/src/main/java/org/endeavourhealth/usermanagermodel/models/caching/UserCache.java
+++ Models/src/main/java/org/endeavourhealth/usermanagermodel/models/caching/UserCache.java
@@ -1,9 +1,12 @@
 package org.endeavourhealth.usermanagermodel.models.caching;
 
 import org.endeavourhealth.common.security.keycloak.client.KeycloakAdminClient;
+import org.endeavourhealth.usermanagermodel.models.json.JsonUser;
 import org.keycloak.representations.idm.UserRepresentation;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UserCache {
@@ -32,6 +35,29 @@ public class UserCache {
         }
 
         return foundUser;
+
+    }
+
+    public static List<JsonUser> getAllUsers() throws Exception {
+        UserRepresentation foundUser = null;
+
+        List<JsonUser> userList = new ArrayList<>();
+        List<UserRepresentation> users;
+        KeycloakAdminClient keycloakClient = new KeycloakAdminClient();
+
+        try {
+            users = keycloakClient.realms().users().getUsers("", 0, 100);
+
+            for (UserRepresentation user : users) {
+                userMap.put(user.getId(), user);
+                userList.add(new JsonUser(user));
+            }
+        } catch (Exception e) {
+
+        }
+
+
+        return userList;
 
     }
 
